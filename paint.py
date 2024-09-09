62e2efc728e3a4e5d6e8bd723755a043668e1e86
Halfa24 <A01738457@tec.mx>
2024-09-09 12:43:43 -0600

    Agregamos color en amarillo

--- paint.py
+++ paint.py
@@ -37,7 +37,6 @@ def square(start, end):
 
 
 def circle(start, end):
-    """Draw circle from start to end."""
     pass  # TODO
 
 
@@ -79,6 +78,7 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
+onkey(lambda: color('yellow'), 'Y')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circle), 'c')
